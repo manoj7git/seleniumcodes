7662cad4ac60234bd186bb1ef3a2b4cf2bb82f6b
manoj7git <43413379+manoj7git@users.noreply.github.com>
2018-10-09 13:18:25 +0530

    upload Docs using Robot class

--- src/com/qa/seleniumtest/uploadDocsUsingRobotclass.java
+++ src/com/qa/seleniumtest/uploadDocsUsingRobotclass.java
@@ -7,17 +7,19 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class iframehandling {
+public class uploadDocsUsingRobotclass {
 
 	public static void main(String[] args) throws InterruptedException, AWTException {
+			
+		//For Upload docs if the Upload/Attach button is not an "Input" tag type 
+		//then one need to use the Robot class inorder to simulate the Keyboard methods and
+		//handle file explorer (non web app.) to uploa docs.
 		
-		Robot rbt = new Robot();
 		
 		System.setProperty("webdriver.gecko.driver", "D:\\me\\Study\\Selenium\\Downloads\\Browser_drivers\\Firefox\\geckodriver-v0.22.0-win64\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
@@ -26,11 +28,13 @@ public class iframehandling {
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		
-		driver.get("https://www.medplusmart.com/pharmaHome");
+		//driver.get("https://www.medplusmart.com/pharmaHome");
+		driver.get("https://www.medplusmart.com/");
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.navigate().refresh();
 		Thread.sleep(3000);
-		driver.findElement(By.linkText("Upload your Prescription")).click();
+		driver.findElement(By.xpath("//img[@title='File Upload']")).click();
+		//driver.findElement(By.linkText("Upload your Prescription")).click();
 		
 		Thread.sleep(3000);
 		WebElement UploadPresc = driver.findElement(By.xpath("//div[@id='uploadPrescriptionModelSec']//h3[@class='color-dred margin-t-none margin-b-20 hidden-xs'][contains(text(),'Upload Prescription')]"));
@@ -39,6 +43,8 @@ public class iframehandling {
 		
 		driver.findElement(By.xpath("//div[@id='uploadPrescriptionModelSec']//span[contains(text(),'Choose File')]")).click();
 		
+		//Accessing the Robot class in order to replicate Keyboard access
+		Robot rbt = new Robot();
 		rbt.setAutoDelay(2000);
 		
 		StringSelection str = new StringSelection("C:\\Users\\manoj.ghadei\\Desktop\\git.jpg");
@@ -63,22 +69,7 @@ public class iframehandling {
 		
 		//driver.findElement(By.xpath("//div[@id='uploadPrescriptionModelSec']//i[@class='closeModal hide-prescriptionblock']")).click();
 		
-		/*
-		Alert popup = driver.switchTo().alert();
-		//Use the ".getText" method to fetch the text appearing on the popup.
-		System.out.println(popup.getText());
-		
-		//Use "Accept" method to click the OK button of the javascript popup/alert 
-		popup.accept();
-		
-		
-		//Use "Dismiss" method to click the "CANCEL" button in the Alert/Popup
-		//popup.dismiss();
-		
-		
-		driver.findElement(By.linkText("Home")).click();
-		System.out.println(driver.getTitle());
-		*/
+
 		driver.quit();
 
 	}
