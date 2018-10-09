6a6e786e8326d98d7a45afac25a78091757fcde1
manoj7git <43413379+manoj7git@users.noreply.github.com>
2018-10-09 18:25:59 +0530

    upload Docs using SendKeys silent method

--- src/com/qa/seleniumtest/uploadDocsUsingRobotclass.java
+++ src/com/qa/seleniumtest/uploadDocsUsingRobotclass.java
@@ -1,7 +1,7 @@
 package com.qa.seleniumtest;
 
 import java.awt.AWTException;
-import java.awt.Robot;
+import java.awt.Robot; //Java AWT (Abstract Window Toolkit) is an API to develop GUI or window-based applications in java
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
@@ -17,8 +17,8 @@ public class uploadDocsUsingRobotclass {
 	public static void main(String[] args) throws InterruptedException, AWTException {
 			
 		//For Upload docs if the Upload/Attach button is not an "Input" tag type 
-		//then one need to use the Robot class inorder to simulate the Keyboard methods and
-		//handle file explorer (non web app.) to uploa docs.
+		//then one need to use the Robot class in order to simulate the Keyboard methods and
+		//handle file explorer (non web app.) to upload docs.
 		
 		
 		System.setProperty("webdriver.gecko.driver", "D:\\me\\Study\\Selenium\\Downloads\\Browser_drivers\\Firefox\\geckodriver-v0.22.0-win64\\geckodriver.exe");
@@ -28,10 +28,9 @@ public class uploadDocsUsingRobotclass {
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		
-		//driver.get("https://www.medplusmart.com/pharmaHome");
 		driver.get("https://www.medplusmart.com/");
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		driver.navigate().refresh();
+		//driver.navigate().refresh();
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//img[@title='File Upload']")).click();
 		//driver.findElement(By.linkText("Upload your Prescription")).click();
@@ -45,29 +44,29 @@ public class uploadDocsUsingRobotclass {
 		
 		//Accessing the Robot class in order to replicate Keyboard access
 		Robot rbt = new Robot();
-		rbt.setAutoDelay(2000);
+		rbt.setAutoDelay(2000);  //Similar to Thread.sleep
 		
+		
+		//StringSelection class has been used to store the doc location string
 		StringSelection str = new StringSelection("C:\\Users\\manoj.ghadei\\Desktop\\git.jpg");
-		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null); //This statement says the above location string is moved to the clipboard.
 		rbt.setAutoDelay(1000);
 		
-		rbt.keyPress(KeyEvent.VK_CONTROL);
+		rbt.keyPress(KeyEvent.VK_CONTROL); //This KeyPress simulates Keyboard Press Ctrl+V action
 		rbt.keyPress(KeyEvent.VK_V);
 		
-		rbt.keyRelease(KeyEvent.VK_CONTROL);
+		rbt.keyRelease(KeyEvent.VK_CONTROL); //This KeyRelease simulates lifting release mouse pointer
 		rbt.keyRelease(KeyEvent.VK_V);
 		rbt.setAutoDelay(1000);
 		
 		rbt.keyPress(KeyEvent.VK_ENTER);
-		rbt.keyRelease(KeyEvent.VK_ENTER);
+		rbt.keyRelease(KeyEvent.VK_ENTER);		
 		
-		//driver.findElement(By.xpath("//div[@id='uploadPrescriptionModelSec']//span[contains(text(),'Choose File')]")).sendKeys("C:\\Users\\manoj.ghadei\\Desktop\\git.jpg");
-		//Thread.sleep(3000);
 		
 		String text1 = driver.findElement(By.xpath("//div[@id='uploadPrescriptionModelSec']//button[@type='button'][contains(text(),'Submit')]")).getText();
 		System.out.println(text1);
 		
-		//driver.findElement(By.xpath("//div[@id='uploadPrescriptionModelSec']//i[@class='closeModal hide-prescriptionblock']")).click();
+		driver.findElement(By.xpath("//div[@id='uploadPrescriptionModelSec']//i[@class='closeModal hide-prescriptionblock']")).click();
 		
 
 		driver.quit();
---
+++ src/com/qa/seleniumtest/uploadDocsUsingSendKeys.java
@@ -0,0 +1,54 @@
+package com.qa.seleniumtest;
+
+import java.awt.AWTException;
+import java.awt.Robot; //Java AWT (Abstract Window Toolkit) is an API to develop GUI or window-based applications in java
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class uploadDocsUsingSendKeys {
+
+	public static void main(String[] args) throws InterruptedException, AWTException {
+			
+		//For Upload docs if the Upload/Attach button is an "Input" tag type 
+		//then one need to use the the sendkeys method just to pass the file to upload location string.
+		
+		
+		System.setProperty("webdriver.gecko.driver", "D:\\me\\Study\\Selenium\\Downloads\\Browser_drivers\\Firefox\\geckodriver-v0.22.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		
+		driver.get("https://cleartax.in/paytax/UploadForm16?ref=income-tax-efiling");
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);		
+		Thread.sleep(3000);
+		
+		String doclocation = "C:\\Users\\manoj.ghadei\\Documents\\Setup.pdf";
+		//WebElement.SendKeys() method to upload the document silently when the Upload/Attach element is an Input type
+		driver.findElement(By.xpath("//input[@id='js-form16-upload-input']")).sendKeys(doclocation);
+		driver.findElement(By.id("dateOfBirth")).sendKeys("10/10/2018");
+		driver.findElement(By.name("submitButton")).click();
+		Thread.sleep(2000);
+		
+		String txt = driver.findElement(By.xpath("//a[@class='ct-bold']")).getText();
+		if (txt.equalsIgnoreCase("Try uploading again")) {
+			System.out.println("The Document was uploaded successfully which made to navigate to the new page");			
+		} else {
+			String errmsg = driver.findElement(By.xpath("//span[contains(text(),'Please select a file to upload')]")).getText();
+			System.out.println("Error message appeared for uploading file" + errmsg);
+
+		}		
+		
+		driver.quit();
+
+	}
+
+}
