b91a087ba966cafbe883318bd408c0e0cfdeb5e1
manoj7git <43413379+manoj7git@users.noreply.github.com>
2018-10-24 15:53:22 +0530

    currentdatetimecode updated

--- src/com/qa/seleniumtest/currentdatetime.java
+++ src/com/qa/seleniumtest/currentdatetime.java
@@ -9,9 +9,12 @@ public class currentdatetime {
 
 	public static void main(String[] args) {
 		
+		//Link for reference
+		//https://www.mkyong.com/java/java-how-to-get-current-date-time-date-and-calender/
+		
 		Calendar cal = Calendar. getInstance();
 		Date date = cal. getTime();
-		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		String formattedDate = dateFormat. format(date);
 		System. out. println("Current time of the day using Calendar - 24 hour format: "+ formattedDate);
 
